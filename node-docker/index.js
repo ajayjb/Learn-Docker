918db809a84f4a1866890b1ffd50b840ca2f421d
ajayjb <ajayjb11@gmail.com>
2025-03-06 16:07:05 +0530

    Feat: Docker In Detail To Next Extent

--- node-docker/index.js
+++ node-docker/index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import cors from "cors";
+import 'dotenv/config'
 
 const app = express();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   return res.status(200).send("Hi there new world!");
 });
 
-app.listen(8080, () => {
-  console.log("Server running on port 8080");
+app.listen(Number(process.env.PORT), () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
