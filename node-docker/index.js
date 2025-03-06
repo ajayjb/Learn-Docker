032163863f8f586794981a7b7dee338934f5851b
ajayjb <ajayjb11@gmail.com>
2025-03-06 23:51:30 +0530

    Feat: Docker Compose

--- node-docker/index.js
+++ node-docker/index.js
@@ -1,13 +1,21 @@
 import express from "express";
 import cors from "cors";
-import 'dotenv/config'
+import "dotenv/config";
 
 const app = express();
 
 app.use(cors());
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   return res.status(200).send("Hi there new world!");
+});
+
+app.get("/getPassword", (req, res) => {
+  return res.status(200).send({ password: process.env.DB_PASSWORD });
+});
+
+app.get("/getToken", (req, res) => {
+  return res.status(200).send({ token: process.env.APP_TOKEN });
 });
 
 app.listen(Number(process.env.PORT), () => {
